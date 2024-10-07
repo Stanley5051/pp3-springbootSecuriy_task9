4f97ff262afa4f67c4ab94df49bd0dbbd49c6f5f
Stanley5051 <timofeev-egor90@yandex.ru>
2024-10-07 16:12:39 +0300

    Add files via upload
    initiall commit

---
+++ Task-9/src/main/java/ru/itmentor/spring/boot_security/demo/controller/AdminController.java
@@ -0,0 +1,67 @@
+package ru.itmentor.spring.boot_security.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import ru.itmentor.spring.boot_security.demo.model.User;
+import ru.itmentor.spring.boot_security.demo.service.RoleService;
+import ru.itmentor.spring.boot_security.demo.service.UserService;
+
+@Controller
+@RequestMapping("/admin/users")
+public class AdminController {
+
+    private final UserService userService;
+    private final RoleService roleService;
+
+    @Autowired
+    public AdminController(UserService userService, RoleService roleService) {
+        this.userService = userService;
+        this.roleService = roleService;
+    }
+
+
+    @GetMapping
+    public String showAllUsers(Model model) {
+        model.addAttribute("users", userService.getAllUsers());
+        return "users";
+    }
+
+    @GetMapping("/{id}")
+    public String showUser(Model model, @PathVariable("id") long id) {
+        model.addAttribute("user", userService.getUser(id));
+        return "user";
+    }
+
+    @GetMapping("/new")
+    public String newUser(@ModelAttribute("user") User user, Model model) {
+        model.addAttribute("roles", roleService.getAllRoles());
+        return "new";
+    }
+
+    @PostMapping
+    public String createUser(@ModelAttribute("user") User user) {
+        userService.createUser(user);
+        return "redirect:/admin/users";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String editUser(Model model, @PathVariable("id") long id) {
+        model.addAttribute("user", userService.getUser(id));
+        model.addAttribute("roles", roleService.getAllRoles());
+        return "edit";
+    }
+
+    @PatchMapping("/{id}")
+    public String updateUser(@ModelAttribute("user") User user, @PathVariable("id") long id) {
+        userService.updateUser(user, id);
+        return "redirect:/admin/users";
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteUser(@PathVariable("id") long id) {
+        userService.deleteUser(id);
+        return "redirect:/admin/users";
+    }
+}
---
+++ Task-9/src/main/java/ru/itmentor/spring/boot_security/demo/service/UserServiceImpl.java
@@ -0,0 +1,46 @@
+package ru.itmentor.spring.boot_security.demo.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ru.itmentor.spring.boot_security.demo.model.User;
+import ru.itmentor.spring.boot_security.demo.repository.UserRepository;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public void createUser(User user) {
+        userRepository.save(user);
+    }
+
+    @Override
+    public User getUser(Long id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public void updateUser(User user, Long id) {
+        user.setId(id);
+        userRepository.save(user);
+    }
+
+
+    @Override
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
+}
